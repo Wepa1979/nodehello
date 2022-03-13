2437188eb6faab12db60568a0493d7b80aeabeaa
Wepa1979 <97387769+Wepa1979@users.noreply.github.com>
2022-03-13 05:08:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
 const http = require('http');
+const axios = require("axios");
+const cheerio = require("cheerio");
 const port = process.env.PORT || 3000;
+
+
+global.utm="";
+global.baha="";
+
+
+async function scrapeData() {
+  try {
+    
+    const url2 = "https://www.lcwaikiki.com/tr-TR/TR/urun/LC-WAIKIKI/erkek/Kazak/5605326/2322591";
+    
+    // Fetch HTML of the page we want to scrape
+    const { data } = await axios.get(url2);
+    // Load HTML we fetched in the previous line
+    const $ = cheerio.load(data);
+    // Select all the list items in plainlist class
+    const listItems = $(".product-title").first().text();
+	
+	const bahasy = $(".price").first().text();
+	
+	
+utm=listItems;
+//baha=bahasy;
+	
+
+} catch (err) {
+    console.error(err);
+  }
+  
+  
+ 
+  
+
+}
+
+
+scrapeData();
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello Node!\n'
-  res.end(msg);
+  res.end(utm);
 });
 
 server.listen(port, () => {
