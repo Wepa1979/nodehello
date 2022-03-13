001fed94c31dc8d343ee4974aa608d8c375fe0f9
Wepa1979 <97387769+Wepa1979@users.noreply.github.com>
2022-03-13 05:29:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,34 +5,9 @@ const cheerio = require("cheerio");
 const port = process.env.PORT || 3000;
 
 
-global.utm="";
-global.baha="";
 
 
-async function scrapeData() {
-  try {
-    
-    const url2 = "https://www.lcwaikiki.com/tr-TR/TR/urun/LC-WAIKIKI/erkek/Kazak/5605326/2322591";
-    
-   
-	
-	
-utm=url2;
-//baha=bahasy;
-	
 
-} catch (err) {
-    console.error(err);
-  }
-  
-  
- 
-  
-
-}
-
-
-//scrapeData();
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
