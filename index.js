deae2fd905ae50a69f1d5d853bfc81b609f6c5cc
Wepa1979 <97387769+Wepa1979@users.noreply.github.com>
2022-03-13 05:24:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -13,17 +13,10 @@ async function scrapeData() {
     
     const url2 = "https://www.lcwaikiki.com/tr-TR/TR/urun/LC-WAIKIKI/erkek/Kazak/5605326/2322591";
     
-    // Fetch HTML of the page we want to scrape
-    const { data } = await axios.get(url2);
-    // Load HTML we fetched in the previous line
-    const $ = cheerio.load(data);
-    // Select all the list items in plainlist class
-    const listItems = $(".product-title").first().text();
-	
-	const bahasy = $(".price").first().text();
+   
 	
 	
-utm=listItems;
+utm=url2;
 //baha=bahasy;
 	
 
