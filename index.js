9694ef134eff286c8b937f4069549e4f43eecd41
Wepa1979 <97387769+Wepa1979@users.noreply.github.com>
2022-03-13 05:04:34 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,12 @@
-var express = require('express');
-const axios = require("axios");
-const cheerio = require("cheerio");
-const fs = require("fs");
+const http = require('http');
+const port = process.env.PORT || 3000;
 
-
-
-var app = express();
-
-global.utm="";
-global.baha="";
-
-
-// URL of the page we want to scrape
-const url = "https://www.lcwaikiki.com/tr-TR/TR/urun/LC-WAIKIKI/erkek/Kazak/5605326/2322591";
-
-// Async function which scrapes the data
-async function scrapeData(url2) {
-  try {
-    // Fetch HTML of the page we want to scrape
-    const { data } = await axios.get(url2);
-    // Load HTML we fetched in the previous line
-    const $ = cheerio.load(data);
-    // Select all the list items in plainlist class
-    const listItems = $(".product-title").first().text();
-	
-	const bahasy = $(".price").first().text();
-	
-	
-utm=listItems;
-//baha=bahasy;
-	
-
-} catch (err) {
-    console.error(err);
-  }
-  
-  
- 
-  
-
-}
-
-
-
-app.get('/:id', function(req, res) {
-	scrapeData(req.param("id"))
-	
-  res.send(utm+"");    
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  const msg = 'Hello Node!\n'
+  res.end(msg);
 });
 
-app.listen(3333);
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
+});
